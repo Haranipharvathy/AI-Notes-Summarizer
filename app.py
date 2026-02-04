82d4eb0e1bbeeb4d81f396034fce4e1183a23450
Haranipharvathy <haranipharvathyr@gmail.com>
2026-02-04 22:04:01 +0530

    app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.title("AI Notes Summarizer")
 
-st.write("get summary of your notes.")
+st.write("Get a quick summary of your notes.")
 
 text = st.text_area("Enter your notes")
 
